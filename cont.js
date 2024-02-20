a3b0f4c034e92dc72e69ea03c788498745b6ea8c
Lotus <75691814+Blak-Lotus@users.noreply.github.com>
2024-02-20 15:24:16 -0500

    Create cont.js

---
+++ cont.js
@@ -0,0 +1,158 @@
+let cg = {
+  1: '#7f0000', 2: '#7d0100', 3: '#7c0200', 4: '#7b0300', 5: '#790500', 6: '#780600', 7: '#770700', 8: '#760800', 9: '#740a00', 10: '#730b00', 11: '#720c00', 12: '#700e00', 13: '#6f0f00', 14: '#6e1000', 15: '#6d1100', 16: '#6b1300', 17: '#6a1400', 18: '#691500', 19: '#671700', 20: '#661800', 21: '#651900', 22: '#641a00', 23: '#621c00', 24: '#611d00', 25: '#601e00', 26: '#5e2000', 27: '#5d2100', 28: '#5c2200', 29: '#5b2300', 30: '#592500', 31: '#582600', 32: '#572700', 33: '#552900', 34: '#542a00', 35: '#532b00', 36: '#522c00', 37: '#502e00', 38: '#4f2f00', 39: '#4e3000', 40: '#4c3200', 41: '#4b3300', 42: '#4a3400', 43: '#493500', 44: '#473700', 45: '#463800', 46: '#453900', 47: '#433b00', 48: '#423c00', 49: '#413d00', 50: '#403e00', 51: '#3e4000', 52: '#3d4100', 53: '#3c4200', 54: '#3b4300', 55: '#394500', 56: '#384600', 57: '#374700', 58: '#354900', 59: '#344a00', 60: '#334b00', 61: '#324c00', 62: '#304e00', 63: '#2f4f00', 64: '#2e5000', 65: '#2c5200', 66: '#2b5300', 67: '#2a5400', 68: '#295500', 69: '#275700', 70: '#265800', 71: '#255900', 72: '#235b00', 73: '#225c00', 74: '#215d00', 75: '#205e00', 76: '#1e6000', 77: '#1d6100', 78: '#1c6200', 79: '#1a6400', 80: '#196500', 81: '#186600', 82: '#176700', 83: '#156900', 84: '#146a00', 85: '#136b00', 86: '#116d00', 87: '#106e00', 88: '#0f6f00', 89: '#0e7000', 90: '#0c7200', 91: '#0b7300', 92: '#0a7400', 93: '#087600', 94: '#077700', 95: '#067800', 96: '#057900', 97: '#037b00', 98: '#027c00', 99: '#017d00', 100: '#007f00'}
+
+let bg = {
+  1: '#f2e6e6', 2: '#f1e6e6', 3: '#f1e6e6', 4: '#f1e6e6', 5: '#f1e6e6', 6: '#f1e6e6', 7: '#f1e6e6', 8: '#f1e6e6', 9: '#f1e6e6', 10: '#f0e7e6', 11: '#f0e7e6', 12: '#f0e7e6', 13: '#f0e7e5', 14: '#f0e7e6', 15: '#f0e7e6', 16: '#f0e7e6', 17: '#f0e7e6', 18: '#f0e7e6', 19: '#efe8e6', 20: '#efe8e6', 21: '#efe8e6', 22: '#efe8e6', 23: '#efe8e6', 24: '#efe8e6', 25: '#efe8e6', 26: '#efe8e6', 27: '#efe8e6', 28: '#eee9e6', 29: '#eee9e6', 30: '#eee9e6', 31: '#eee9e6', 32: '#eee9e6', 33: '#eee9e6', 34: '#eee9e6', 35: '#eee9e6', 36: '#eeeae6', 37: '#edeae6', 38: '#edeae6', 39: '#edeae6', 40: '#edeae6', 41: '#edeae6', 42: '#edeae6', 43: '#edeae6', 44: '#edeae6', 45: '#ecebe6', 46: '#ecebe6', 47: '#ecebe6', 48: '#ecebe6', 49: '#ecebe6', 50: '#ecebe6', 51: '#ecebe6', 52: '#ecebe6', 53: '#ecebe6', 54: '#ebece6', 55: '#ebece6', 56: '#ebece6', 57: '#ebece6', 58: '#ebece6', 59: '#ebece6', 60: '#ebece6', 61: '#ebece6', 62: '#ebece6', 63: '#eaede6', 64: '#eaede6', 65: '#eaede6', 66: '#eaede6', 67: '#eaede6', 68: '#eaede6', 69: '#eaede6', 70: '#eaede6', 71: '#eaeee6', 72: '#e9eee6', 73: '#e9eee6', 74: '#e9eee6', 75: '#e9eee6', 76: '#e9eee6', 77: '#e9eee6', 78: '#e9eee6', 79: '#e9eee6', 80: '#e8efe6', 81: '#e8efe6', 82: '#e8efe6', 83: '#e8efe6', 84: '#e8efe6', 85: '#e8efe6', 86: '#e8efe6', 87: '#e8efe6', 88: '#e8efe6', 89: '#e7f0e6', 90: '#e7f0e6', 91: '#e7f0e6', 92: '#e7f0e6', 93: '#e7f0e6', 94: '#e7f0e6', 95: '#e7f0e6', 96: '#e7f0e6', 97: '#e7f0e6', 98: '#e6f1e6', 99: '#e6f1e6', 100: '#e6f1e6'}
+
+let numToLetters = {
+  0: " F ",
+  1: " F ",
+  2: " F ",
+  3: " F ",
+  4: " F ",
+  5: " F ",
+  6: " F ",
+  7: " F ",
+  8: " F ",
+  9: " F ",
+  10: " F ",
+  11: " F ",
+  12: " F ",
+  13: " F ",
+  14: " F ",
+  15: " F ",
+  16: " F ",
+  17: " F ",
+  18: " F ",
+  19: " F ",
+  20: " F ",
+  21: " F ",
+  22: " F ",
+  23: " F ",
+  24: " F ",
+  25: " F ",
+  26: " F ",
+  27: " F ",
+  28: " F ",
+  29: " F ",
+  30: " F ",
+  31: " F ",
+  32: " F ",
+  33: " F ",
+  34: " F ",
+  35: " F ",
+  36: " F ",
+  37: " F ",
+  38: " F ",
+  39: " F ",
+  40: " F ",
+  41: " F ",
+  42: " F ",
+  43: " F ",
+  44: " F ",
+  45: " F ",
+  46: " F ",
+  47: " F ",
+  48: " F ",
+  49: " F ",
+  50: " F ",
+  51: " F ",
+  52: " F ",
+  53: " F ",
+  54: " F ",
+  55: " F ",
+  56: " F ",
+  57: " F ",
+  58: " F ",
+  59: " F ",
+  60: " D ",
+  61: " D ",
+  62: " D ",
+  63: " D ",
+  64: " D ",
+  65: " D ",
+  66: " D ",
+  67: " D+ ",
+  68: " D+ ",
+  69: " D+ ",
+  70: " C ",
+  71: " C ",
+  72: " C ",
+  73: " C ",
+  74: " C ",
+  75: " C ",
+  76: " C ",
+  77: " C+ ",
+  78: " C+ ",
+  79: " C+ ",
+  80: " B ",
+  81: " B ",
+  82: " B ",
+  83: " B ",
+  84: " B ",
+  85: " B ",
+  86: " B ",
+  87: " B+ ",
+  88: " B+ ",
+  89: " B+ ",
+  90: " A ",
+  91: " A ",
+  92: " A ",
+  93: " A ",
+  94: " A ",
+  95: " A ",
+  96: " A ",
+  97: " A ",
+  98: " A ",
+  99: " A ",
+  100: " A "
+}
+
+let gd = window.location.search.split("?")[1].split("&")
+
+function change(n, l, p){
+  document.querySelectorAll('.letter-container')[n].querySelector('div').querySelector('div').querySelector('div').innerHTML = `${l}<span class="percent"> ${p}% </span>`
+  document.querySelectorAll('.letter-container')[n].querySelector('div').style=`color:${cg[p]};background-color:${bg[p]}`
+}
+
+try {
+  if (document.location.origin + document.location.pathname != "https://gb.browardschools.com/Pinnacle/Gradebook/InternetViewer/GradeReport.aspx"){
+    throw "milTools-GradeFix: disabled, wrong website"
+  }
+  document.body.style = "display: none;"
+  try {
+    if (gd){
+      let overrides = gd
+      setTimeout(()=>{document.body.style = "display: block;"},1e3)
+      for (let key in overrides) {
+        change(key-1, numToLetters[overrides[key]], overrides[key])
+      }
+    }
+    console.log("milTools-GradeFix: Loaded, using dynamic entry")
+  } catch (e) {
+    setTimeout(()=>{document.body.style = "display: block;"},1e3)
+    let overrides = {
+      1: 71,
+      3: 88,
+      8: 84
+    }
+    for (let key in overrides) {
+      change(key-1, numToLetters[overrides[key]], overrides[key])
+    }
+    console.log("milTools-GradeFix: Loaded, using predefined entry")
+  }
+} catch (e) {
+  console.log(e)
+}
+
+
+
+
+
+
+
+
+
+
+
+
